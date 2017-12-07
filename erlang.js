0a80220e3a4bd99c1adabaa2950c515c849aebc0
ElliotShin <wodshin@gmail.com>
2017-12-08 02:45:22 +0900

    Create erlang.js

---
+++ erlang.js
@@ -0,0 +1,26 @@
+CodeHighlighter.addStyle("erlang",{
+  punctuation: {
+    exp: /-(&gt;)?|;|\||\!/
+  },
+  comment : {
+    exp  : /%[^\n]+/
+  },
+  bracket : {
+    exp  : /\(|\)|{|}|(&gt;&gt;)|(&lt;&lt;)/
+  },
+  string : {
+    exp  : /"[^"]*"/
+  },
+  keyword : {
+    exp  : /\b(after|and|andalso|band|begin|bnot|bor|bsl|bsr|bxor|case|catch|cond|div|end|fun|if|let|not|of|or|orelse|query|receive|rem|try|when|xor)\b/
+  },
+  pp : {
+    exp : /\b(module|author|include|include_lib|behavior|behaviour|define|export)\b/
+  },
+  atom : {
+    exp  : /\b([a-z][@\w]*|'[^']*')\b/
+  },
+  variable : {
+    exp : /\b(_|_?[A-Z][@\w]*)\b/
+  }
+});
